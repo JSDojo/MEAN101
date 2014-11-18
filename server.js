4bbad4cbf16344a3dcc3aa1b364406cf48a9391b
ArturoMC <=>
2014-11-18 12:54:52 -0600

    Base server init and additional ignore rules

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// Here we will define our server structure and dispatch our server events
+// Defining our express and server instance
+var express = require('express'),
+    server  = express();
+
+var port    = process.env.PORT || 3000;
+
+// Defining the default route to dislpay views whenever the user lands on "host:port/"
+server.use('/', express.static(__dirname+"/public/"));
+
+server.listen(port, function(){
+    console.log("Server up and running on : " + port);
+});
